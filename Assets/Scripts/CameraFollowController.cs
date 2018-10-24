d4e2ad3a770101448a03d1aff9692f648155e445
JonathanLauwers <Lauwersjon@gmail.com>
2018-10-24 15:50:09 +0200

    Movement of car + rotation of wheels

---
+++ Assets/Scripts/CameraFollowController.cs
@@ -0,0 +1,34 @@
+﻿    using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour
+{
+
+    public Transform objectToFollow;
+    public Vector3 offset;
+    public float followSpeed = 10f;
+    public float lookSpeed = 10f;
+
+    public void LookAtTarget()
+    {
+        Vector3 _LookDirection = objectToFollow.position - transform.position;
+        Quaternion _rot = Quaternion.LookRotation(_LookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
+    }
+
+    public void MoveToTarget()
+    {
+        Vector3 _targetPos = objectToFollow.position +
+                             objectToFollow.forward * offset.z +
+                             objectToFollow.right * offset.x +
+                             objectToFollow.up * offset.y;
+        transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+}
---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    public WheelCollider frontDriverW, frontPassengerW;
+    public WheelCollider rearDriverW, rearPassengerW;
+    public Transform frontDriverT, frontPassengerT;
+    public Transform rearDriverT, rearPassengerT;
+    public float maxSteerAngle = 30;
+    public float motorForce = 50;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float steeringAngle;
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void Steer()
+    {
+        steeringAngle = maxSteerAngle * horizontalInput;
+        frontDriverW.steerAngle = steeringAngle;
+        frontPassengerW.steerAngle = steeringAngle;
+    }
+
+    private void Accelerate()
+    {
+        frontDriverW.motorTorque = verticalInput * motorForce * 10;
+        frontPassengerW.motorTorque = verticalInput * motorForce * 10;
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontDriverW, frontDriverT);
+        UpdateWheelPose(frontPassengerW, frontPassengerT);
+        UpdateWheelPose(rearDriverW, rearDriverT);
+        UpdateWheelPose(rearPassengerW, rearPassengerT);
+    }
+
+    private void UpdateWheelPose(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 _pos = _transform.position;
+        Quaternion _quat = _transform.rotation;
+
+        _collider.GetWorldPose(out _pos, out _quat);
+
+        _transform.position = _pos;
+        _transform.rotation = _quat;
+    }
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+    }
+
+}
