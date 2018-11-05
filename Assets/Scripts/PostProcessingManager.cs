7a8fe9febacb1d75779568f801f323645b2a7644
JonasKempeneers <jonaskempeneers193@gmail.com>
2018-11-05 15:43:43 +0100

    Added post processing stack and drunk effect
    I added the post processing stack from Unity and added some effects to create a drunk effect. The effect now starts when you press the 'd' button.

---
+++ Assets/Scripts/PostProcessingManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+
+public class PostProcessingManager : MonoBehaviour
+{
+    private PostProcessingBehaviour DrunkProfile;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    DrunkProfile = gameObject.GetComponent<PostProcessingBehaviour>();
+	    DrunkProfile.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.D))
+	    {
+            toggleDrunkMode();
+	    }
+	}
+
+    public void toggleDrunkMode()
+    {
+        DrunkProfile.enabled = !DrunkProfile.enabled;
+    }
+}
