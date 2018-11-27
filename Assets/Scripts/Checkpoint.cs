e5de50cd51f26720ea1682060e5a4101bf937972
JonathanLauwers <Lauwersjon@gmail.com>
2018-11-27 11:02:45 +0100

    Implementing checkpoint reset + simpel steeringwheel
    Co-Authored-By: steffenlen <steffenlen@users.noreply.github.com>
    Co-Authored-By: JonasKempeneers <jonaskempeneers@users.noreply.github.com>
    Co-Authored-By: hakanpinar <hakanpinar@users.noreply.github.com>

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -9,21 +9,39 @@ public class Checkpoint : MonoBehaviour
     void Start()
     {
         cpm = GameObject.FindGameObjectWithTag("CheckpointMaster").GetComponent<CheckpointMaster>();
+
+        foreach (GameObject checkpoint in cpm.checkpoints)
+        {
+            checkpoint.GetComponent<Renderer>().material.color = Color.clear;
+
+        }
+
         cpm.nextCheckpoint = cpm.checkpoints[0];
+        cpm.nextCheckpoint.GetComponent<Renderer>().material.color = Color.green;
     }
-    
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             if ((cpm.nextCheckpoint).transform.position == transform.position)
             {
-                //cpm.lastCheckPointPos = transform.position;
-                cpm.currentCheckpoint += 1;
-                cpm.nextCheckpoint = cpm.checkpoints[cpm.currentCheckpoint];
-                this.GetComponent<Renderer>().material.color = Color.green;
+                cpm.checkpoints[cpm.currentCheckpoint].GetComponent<Renderer>().material.color = Color.clear;
+
+                if (cpm.currentCheckpoint+1 == cpm.checkpoints.Length)
+                {
+                    cpm.currentCheckpoint = 0;
+                    cpm.nextCheckpoint = cpm.checkpoints[0];
+                }
+                else
+                {
+                    cpm.currentCheckpoint += 1;
+                    cpm.nextCheckpoint = cpm.checkpoints[cpm.currentCheckpoint];
+                }
+
+                cpm.nextCheckpoint.GetComponent<Renderer>().material.color = Color.green;
             }
-            
+
         }
         
 
