fd299e56c0a4d3c82c4b68be7b4ca3950063359e
steffenlen <steffen.lenaerts@student.pxl.be>
2018-12-02 12:00:42 +0100

    Added various timers
    -Added global timer
    -Added timer per round
    -Fixed countdowntimer
    -Added laps

--- Assets/Scripts/CountDownScript.cs
+++ Assets/Scripts/CountDownScript.cs
@@ -7,69 +7,111 @@ using System;
 
 public class CountDownScript : MonoBehaviour {
 
-    float timeLeft = 3.0f;
+    private float timeLeft = 3.0f;
 
     public Text countDownText;
     public Text timerText;
+    public Text currentLapText;
 
     public bool PassedLastCheckPoint = false;
 
+
     private Stopwatch stopwatch;
-
-
+    private Stopwatch roundwatch;
     private GameObject car;
     private Car carScript;
     private Checkpoint checkPointScript;
 
     private GameObject checkpoint;
+    
+    private int currentRound;
+    private int maxRound = 3;
 
-	// Use this for initialization
-	void Start () {
+ 
+
+
+    private int hours;
+    private int minutes;
+    private int seconds;
+    private int milliseconds;
+
+    private bool test = false;
+
+    // Use this for initialization
+    void Start () {
         car = GameObject.Find("Car");
         carScript = car.GetComponent<Car>();
 
         stopwatch = new Stopwatch();
-
-
-       // checkpoint = GameObject.Find("Checkpoint1");
-        //checkPointScript = checkpoint.GetComponent<Checkpoint>();
-
-	}
+        roundwatch = new Stopwatch();
+        currentRound = 0;
+              
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
         timeLeft -= Time.deltaTime;
-        countDownText.text = Mathf.Round(timeLeft).ToString();
-        if(timeLeft < 0)
+      
+        if (timeLeft < 0 && test == false)
         {
             countDownText.fontStyle = FontStyle.Bold;
-            countDownText.text = "go!";
+            countDownText.text = "Go!";
             carScript.canControl = true;
 
+            test = true;
             stopwatch.Start();
+            roundwatch.Start();
+
             Invoke("DisableCountDownText", 2f);
         }
+        else if(timeLeft > 0)
+        {
+            countDownText.text = Mathf.Round(timeLeft).ToString();
+          
+        }
 
-       
 
         if (PassedLastCheckPoint == true)
         {
-            stopwatch.Stop();
-            
-            // timerText.text = ts.ToString();
+            currentRound++;
+            TimeSpan ts = roundwatch.Elapsed;
 
-        }else
+            hours = ts.Hours;
+            minutes = ts.Minutes;
+            seconds = ts.Seconds;
+            milliseconds = ts.Milliseconds;
+            PassedLastCheckPoint = false;
+
+            currentLapText.text = string.Format("Lap {0}: {1:00}:{2:00}:{3:00}.{4:00}", currentRound, hours, minutes, seconds, milliseconds);
+
+            if (currentRound >= maxRound)
+            {
+                stopwatch.Stop();
+                roundwatch.Stop();
+            }
+            else
+            {
+                roundwatch.Reset();
+                roundwatch.Start();
+            }
+
+        }
+        else
         {
             TimeSpan ts = stopwatch.Elapsed;
             timerText.text = ts.ToString();
         }
-                
 
-     }
+
+
+
+    }
+
 
     private void DisableCountDownText()
     {
         countDownText.enabled = false;
     }
+    
 }
